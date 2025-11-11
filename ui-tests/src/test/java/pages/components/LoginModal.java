cd38259b05b15c763e9c26a5b805e0f60deb1842
Kostiantyn Herasymets <ko.herasymets@gmail.com>
2025-11-11 21:57:59 +0100

    Revert "Merge pull request #9 from sdet21organization/feature/ui-tests-refactor"
    This reverts commit 1b9b30e9ba7fcbd7d80dc1fd89a9d042761faa7f, reversing
    changes made to 9acef82a2f52fab9c9ac042855b4d8cb1aae448c.

--- ui-tests/src/test/java/pages/components/LoginModal.java
+++ ui-tests/src/test/java/pages/components/LoginModal.java
@@ -1,42 +1,21 @@
 package pages.components;
 
 import com.microsoft.playwright.Locator;
-import com.microsoft.playwright.options.WaitForSelectorState;
 import context.TestContext;
 import io.qameta.allure.Step;
+import pages.BasePage;
 
-public class LoginModal {
-    private final TestContext context;
-    private final Locator root;
-    private final Locator usernameInput;
-    private final Locator passwordInput;
-    private final Locator submitButton;
+public class LoginModal extends BasePage {
+
+    public final Locator loginFormPopup;
 
     public LoginModal(TestContext context) {
-        this.context = context;
-        this.root = context.page.locator("form.login").first();
-        this.usernameInput = root.locator("input#username, input[name='username'], input[name='log']").first();
-        this.passwordInput = root.locator("input#password, input[name='password'], input[name='pwd']").first();
-        this.submitButton = root.locator("button[name='login'], button[type='submit'], input[type='submit']").first();
+        super(context);
+        this.loginFormPopup = context.page.locator("#login-form-popup");
     }
 
-    @Step("Wait login modal visible")
-    public void waitVisible() {
-        root.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
-    }
-
-    public boolean isVisible() {
-        return root.isVisible();
-    }
-
+    @Step("Check if Login modal is opened")
     public boolean LoginModalIsOpened() {
-        return isVisible();
-    }
-
-    @Step("Login")
-    public void login(String email, String password) {
-        usernameInput.fill(email);
-        passwordInput.fill(password);
-        submitButton.click();
+        return loginFormPopup.isVisible();
     }
 }
